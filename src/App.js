d2f415927bac751adbfbe2c7656d88b8d499109f
Dilip shital <55436749+dilip9066@users.noreply.github.com>
2023-03-20 17:02:19 +0530

    office work

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import "./App.css";
-import Header from "./Components/Dashboard_Components/Header";
-import Sidebar from "./Components/Dashboard_Components/Sidebar";
-import DashboardCards from "./Components/Dashboard_Components/DashboardCards";
+
 import Profile from "./Components/Dashboard_Components/Profile";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Layout from "./Components/Dashboard_Components/Layout";
 import Forms from "./Components/Dashboard_Components/Forms";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Errorpage from "./Components/Dashboard_Components/Errorpage";
+
 function App() {
   return (
-    <>
-      <Router>
-        {/* <Header />
-        <Sidebar /> */}
-
-        <Routes>
-          <Route path="/dashboard" element={<DashboardCards />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/forms" element={<Forms />} />
-        </Routes>
-      </Router>
-      <DashboardCards />
-
-      <Profile />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/layout" element={<Layout />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/forms" element={<Forms />} />
+        <Route path="*" elememt={<Errorpage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Dashboard_Components/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+export default function Layout() {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <Outlet/>
+    </>
+  );
+}
